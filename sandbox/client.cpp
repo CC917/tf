692dd2d85a991232ae9f24e8a57482d1d55592de
CC917 <3047089565@qq.com>
2022-12-05 16:51:54 +0800

    first build version

--- sandbox/client.cpp
+++ sandbox/client.cpp
@@ -3,8 +3,17 @@
 using std::cout;
 using std::endl;
 
+#include "l0-infra/common/web/cpp-httplib/httplib.h"
+
 int main()
 {
 	cout << "client sandbox" << endl;
+
+	httplib::Client cli("http://www.baidu.com");
+
+	auto res = cli.Get("/");
+	cout << "ret code = " << res->status << endl;
+	cout << "ret body = " << res->body << endl;
+
 	return 0;
 }
